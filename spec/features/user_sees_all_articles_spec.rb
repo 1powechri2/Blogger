1195281ae12ea0bdc54d9e891eba049595be1110
Chris Powell <chris.ian.powell@gmail.com>
2018-07-05 14:29:08 -0600

    adds links for articles in index

--- spec/features/user_sees_all_articles_spec.rb
+++ spec/features/user_sees_all_articles_spec.rb
@@ -11,5 +11,14 @@ describe "user sees all articles" do
       expect(page).to have_content(article_1.title)
       expect(page).to have_content(article_2.title)
     end
+    it 'has links to indivdual articles by title' do
+      article_1 = Article.create!(title: "Title 1", body: "Body 1")
+      article_2 = Article.create!(title: "Title 2", body: "Body 2")
+
+      visit '/articles'
+
+      expect(page).to have_link(article_1.title)
+      expect(page).to have_link(article_2.title)
+    end
   end
 end
