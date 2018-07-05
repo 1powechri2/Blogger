a9b08b7b6f9c63e301e3f2c04b879f43d981ac5b
Chris Powell <chris.ian.powell@gmail.com>
2018-07-05 14:56:40 -0600

    adds test for show view being access through index link

---
+++ spec/features/user_sees_individual_articles.rb
@@ -0,0 +1,16 @@
+require 'rails helper'
+
+describe 'User visits individual article page' do
+  describe 'that they have accessed by link via the index page' do
+    it 'displays the information for one article' do
+      article = Article.create!(title: "New Title", body: "New Body")
+
+      visit articles_path
+
+      click_link article.title
+
+      expect(page).to have_content(article.title)
+      expect(page).to have_content(article.body)
+    end
+  end
+end
